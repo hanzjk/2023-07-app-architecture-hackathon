1e6aa7c51a80c5796c91edf7eb86f2326a595d97
hanzjk <hansijk@yahoo.com>
2023-07-31 16:26:19 +0530

    Change asgardeo config for web app

--- resident-webapp/src/App.tsx
+++ resident-webapp/src/App.tsx
@@ -12,8 +12,8 @@ import Login from './pages/Login';
 import ViewActualVisit from './pages/ViewActualVisit';
 
 const authConfig = {
-    "baseUrl": "https://api.asgardeo.io/t/architecturemindmeld",
-    "clientID": "nzsVQ5DJMfS8530VnrhJufYXTJMa",
+    "baseUrl": "https://api.asgardeo.io/t/choreoplayground",
+    "clientID": "pWQ5IqKuIplCovSiULd94FuYayoa",
     "scope": [
         "openid",
         "profile",
