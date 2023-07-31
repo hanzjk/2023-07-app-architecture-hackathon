23be4155c21a85fa1cf89aeecc0ed319ca0a6be3
hanzjk <hansijk@yahoo.com>
2023-07-31 14:26:21 +0530

    Add baseURL as a configuration

--- resident-webapp/src/api.ts
+++ resident-webapp/src/api.ts
@@ -8,6 +8,6 @@ export function setAccessToken(token: string) {
 }
 
 export const API = axios.create({
-    baseURL: 'https://1698d406-65dc-49be-b615-ab2451a002b1-dev.e1-us-east-azure.choreoapis.dev/qfxg/visit-service/visit-420/1.0.0'
+    baseURL: window.config.baseURL
    
 });
